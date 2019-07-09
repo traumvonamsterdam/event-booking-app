ffc49b7ed1143ecc1192496d6591f0cd2f8e691c
James Leung <jleung128hk@gmail.com>
2019-07-09 10:09:19 -0400

    Add schemas and resolvers for events and bookings

--- app.js
+++ app.js
@@ -1,49 +1,17 @@
 const express = require("express");
 const graphqlHttp = require("express-graphql");
-const { buildSchema } = require("graphql");
-const graphql_schema = require("./graphql-schema");
 const mongoose = require("mongoose");
 
-const Event = require("./models/event");
+const gqlSchema = require("./graphql/schema/index");
+const gqlResolvers = require("./graphql/resolvers/index");
 
 const app = express();
 
 app.use(
   "/graphql",
   graphqlHttp({
-    schema: buildSchema(graphql_schema),
-    rootValue: {
-      events: () =>
-        Event.find()
-          .then(events =>
-            events.map(event => ({
-              ...event._doc,
-              _id: event.id
-            }))
-          )
-          .catch(err => {
-            throw err;
-          }),
-      createEvent: args => {
-        const { title, description, price, date } = args.eventInput;
-        const event = new Event({
-          title,
-          description,
-          price,
-          date: new Date(date)
-        });
-        return event
-          .save()
-          .then(result => {
-            console.log(result);
-            return { ...result._doc, _id: result.id };
-          })
-          .catch(err => {
-            console.log(err);
-            throw err;
-          });
-      }
-    },
+    schema: gqlSchema,
+    rootValue: gqlResolvers,
     graphiql: true
   })
 );
--- models/event.js
+++ models/event.js
@@ -15,6 +15,10 @@ const eventSchema = new Schema({
   date: {
     type: Date,
     required: true
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
   }
 });
 
